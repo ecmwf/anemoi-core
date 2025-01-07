9866923f3e9eb755b60d4d43cde64544e17b2fd1
Florian Prill <63-m300196@users.noreply.gitlab.dkrz.de>
2025-01-07 13:18:54 +0100

    [fix] avoid accessing potentially non-existing property `supporting_arrays`.

--- training/src/anemoi/training/data/datamodule.py
+++ training/src/anemoi/training/data/datamodule.py
@@ -79,7 +79,9 @@ class AnemoiDatasetsDataModule(pl.LightningDataModule):
 
     @cached_property
     def supporting_arrays(self) -> dict:
-        return self.ds_train.supporting_arrays | self.grid_indices.supporting_arrays
+        if hasattr(self.ds_train, "supporting_arrays"):
+            return self.ds_train.supporting_arrays | self.grid_indices.supporting_arrays
+        return self.grid_indices.supporting_arrays
 
     @cached_property
     def data_indices(self) -> IndexCollection:
