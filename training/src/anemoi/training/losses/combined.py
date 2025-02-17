91e33ead08a615e21b05dd8f7a90a46a83f0321f
Harrison Cook <harrison@eldrest.com>
2025-02-17 23:24:17 +0000

    Improve docstrings

--- training/src/anemoi/training/losses/combined.py
+++ training/src/anemoi/training/losses/combined.py
@@ -32,9 +32,9 @@ class CombinedLoss(BaseWeightedLoss):
 
     def __init__(
         self,
-        *extra_losses: dict[str, Any] | Callable,
+        *extra_losses: dict[str, Any] | Callable | BaseWeightedLoss,
         loss_weights: tuple[int, ...] | None = None,
-        losses: tuple[dict[str, Any] | Callable] | None = None,
+        losses: tuple[dict[str, Any] | Callable | BaseWeightedLoss] | None = None,
         **kwargs,
     ):
         """Combined loss function.
@@ -42,14 +42,32 @@ class CombinedLoss(BaseWeightedLoss):
         Allows multiple losses to be combined into a single loss function,
         and the components weighted.
 
+        As the losses are designed for use within the context of the 
+        anemoi-training configuration, `losses` work best as a dictionary.
+
+        If `losses` is a `tuple[dict]`, the `scalars` key will be extracted
+        before being passed to `get_loss_function`, and the `scalars` defined
+        in each loss only applied to the respective loss. Thereby `scalars`
+        added to this class will be routed correctly.
+        If `losses` is a `tuple[Callable]`, all `scalars` added to this class
+        will be added to all underlying losses.
+        And if `losses` is a `tuple[WeightedLoss]`, no scalars added to
+        this class will be added to the underlying losses, as it is 
+        assumed that will be done by the parent function.
+
         Parameters
         ----------
-        losses: tuple[dict[str, Any]]
-            Tuple of losses to initialise with `GraphForecaster.get_loss_function`.
-            Allows for kwargs to be passed, and weighings controlled.
-            If a loss should only have some of the scalars, set `scalars` in the loss config.
-            If no scalars are set, all scalars will be included.
-        *extra_losses: dict[str, Any]
+        losses: tuple[dict[str, Any] | Callable | BaseWeightedLoss],
+            if a `tuple[dict]`:
+                Tuple of losses to initialise with `GraphForecaster.get_loss_function`.
+                Allows for kwargs to be passed, and weighings controlled.
+                If a loss should only have some of the scalars, set `scalars` in the loss config.
+                If no scalars are set, all scalars added to this class will be included.
+            if a `tuple[Callable]`:
+                Will be called with `kwargs`, and all scalars added to this class added.
+            if a `tuple[BaseWeightedLoss]`:
+                Added to the loss function, and no scalars passed through.
+        *extra_losses: dict[str, Any]  | Callable | BaseWeightedLoss],
             Additional arg form of losses to include in the combined loss.
         loss_weights : optional, tuple[int, ...] | None
             Weights of each loss function in the combined loss.
@@ -57,7 +75,7 @@ class CombinedLoss(BaseWeightedLoss):
             If None, all losses are weighted equally.
             by default None.
         kwargs: Any
-            Additional arguments to pass to the loss functions
+            Additional arguments to pass to the loss functions, if not Loss.
 
         Examples
         --------
@@ -66,6 +84,13 @@ class CombinedLoss(BaseWeightedLoss):
                 loss_weights=(1.0,),
                 node_weights=node_weights
             )
+        >>> CombinedLoss(
+                {"_target_": "anemoi.training.losses.mse.WeightedMSELoss", "scalars":['scalar_1']},
+                loss_weights=(1.0,),
+                node_weights=node_weights
+            )
+            CombinedLoss.add_scalar(name = 'scalar_1', ...)
+            # Only added to the `WeightedMSELoss` if specified in it's `scalars`.
         --------
         >>> CombinedLoss(
                 losses = [anemoi.training.losses.mse.WeightedMSELoss],
@@ -82,6 +107,7 @@ class CombinedLoss(BaseWeightedLoss):
                 - _target_: anemoi.training.losses.mae.WeightedMAELoss
             scalars: ['*']
             loss_weights: [1.0, 0.6]
+            # All scalars passed to this class will be added to each underlying loss
         ```
 
         ```
@@ -94,6 +120,7 @@ class CombinedLoss(BaseWeightedLoss):
                   scalars: ['atmosphere']
             scalars: ['*']
             loss_weights: [1.0, 1.0]
+            # Only the specified scalars will be added to each loss
         ```
         """
         super().__init__(node_weights=None)
@@ -109,15 +136,19 @@ class CombinedLoss(BaseWeightedLoss):
         assert len(losses) > 0, "At least one loss must be provided"
 
         for i, loss in enumerate(losses):
-            self._loss_scalar_specification[i] = loss.pop("scalars", ["*"])
 
-            self.losses.append(
-                (
-                    GraphForecaster.get_loss_function(loss, scalars={}, **dict(kwargs))
-                    if isinstance(loss, (DictConfig, dict))
-                    else loss(**kwargs)
-                ),
-            )
+            if isinstance(loss, (DictConfig, dict)):
+                self._loss_scalar_specification[i] = loss.pop("scalars", ["*"])
+                self.losses.append(
+                        GraphForecaster.get_loss_function(loss, scalars={}, **dict(kwargs))
+                )
+            elif isinstance(loss, Callable):
+                self._loss_scalar_specification[i] = ["*"]
+                self.losses.append(loss(**kwargs))
+            else:
+                self._loss_scalar_specification[i] = []
+                self.losses.append(loss)
+
             self.add_module(self.losses[-1].name + str(i), self.losses[-1])
         self.loss_weights = loss_weights
 
