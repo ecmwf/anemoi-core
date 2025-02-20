3666847e61dc06d7a54b65a6f49983e035c4f2d6
theissenhelen <helen.theissen@ecmwf.int>
2025-02-20 11:19:02 +0000

    fix: adjust to hydra by using --config-name

--- training/src/anemoi/training/schemas/dataloader.py
+++ training/src/anemoi/training/schemas/dataloader.py
@@ -120,15 +120,15 @@ class DataLoaderSchema(PydanticBaseModel):
     "Per-GPU batch size."
     limit_batches: LoaderSet = Field(example=None)
     "Limit number of batches to run. Default value null, will run on all the batches."
-    training: DatasetSchema | DictConfig
+    training: Union[DatasetSchema, DictConfig]
     "Training DatasetSchema."
-    validation: DatasetSchema | DictConfig
+    validation: Union[DatasetSchema, DictConfig]
     "Validation DatasetSchema."
-    test: DatasetSchema | DictConfig
+    test: Union[DatasetSchema, DictConfig]
     "Test DatasetSchema."
     validation_rollout: PositiveInt = Field(example=1)
     "Number of rollouts to use for validation, must be equal or greater than rollout expected by callbacks."
-    # TODO(Helen): Ccheck that this equal or greater than the number of rollouts expected by callbacks ???
+    # TODO(Helen): Check that this equal or greater than the number of rollouts expected by callbacks ???
     read_group_size: PositiveInt = Field(example=None)
     "Number of GPUs per reader group. Defaults to number of GPUs (see BaseSchema validators)."
     grid_indices: Union[FullGridIndicesSchema, MaskedGridIndicesSchema]
