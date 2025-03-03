f87e5061b5e402084558961cabd4bd2a62d0bb58
Vera Gahlen <vera.gahlen@ecmwf.int>
2025-03-03 11:51:29 +0000

    clean up

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -18,7 +18,9 @@ from omegaconf import OmegaConf
 def architecture_config(request) -> None:
     overrides = request.param
     with initialize(version_base=None, config_path="", job_name="test_basic"):
-        template = compose(config_name="config", overrides=overrides) # apply architecture overrides to template since they override a default
+        template = compose(
+            config_name="config", overrides=overrides
+        )  # apply architecture overrides to template since they override a default
         global_modifications = compose(config_name="test_training_cycle")
         specific_modifications = compose(config_name="test_basic")
         cfg = OmegaConf.merge(template, global_modifications, specific_modifications)
