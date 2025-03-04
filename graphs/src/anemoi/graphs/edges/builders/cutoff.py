fix: masking was missing

graphs/src/anemoi/graphs/edges/builders/cutoff.py
@@ -23,7 +23,6 @@ from torch_geometric.data.storage import NodeStorage
 from anemoi.graphs import EARTH_RADIUS
 from anemoi.graphs.edges.builders.base import BaseEdgeBuilder
 from anemoi.graphs.edges.builders.masking import NodeMaskingMixin
-from anemoi.graphs.generate.transforms import latlon_rad_to_cartesian
 from anemoi.graphs.utils import get_grid_reference_distance
 
 TORCH_CLUSTER_AVAILABLE = find_spec("torch_cluster") is not None
@@ -114,12 +113,10 @@ class CutOffEdges(BaseEdgeBuilder, NodeMaskingMixin):
     def _compute_edge_index_pyg(self, source_nodes: NodeStorage, target_nodes: NodeStorage) -> torch.Tensor:
         from torch_cluster.radius import radius
 
-        edge_index = radius(
-            latlon_rad_to_cartesian(source_nodes.x.to(self.device)),
-            latlon_rad_to_cartesian(target_nodes.x.to(self.device)),
-            r=self.radius,
-            max_num_neighbors=self.max_num_neighbours,
-        )
+        source_coords, target_coords = self.get_cartesian_node_coordinates(source_nodes, target_nodes)
+
+        edge_index = radius(source_coords, target_coords, r=self.radius, max_num_neighbors=self.max_num_neighbours)
+    
         return torch.flip(edge_index, [0])
 
     def _crop_to_max_num_neighbours(self, adjmat):
@@ -148,10 +145,11 @@ class CutOffEdges(BaseEdgeBuilder, NodeMaskingMixin):
         return coo_matrix((adjmat.data[mask], (adjmat.row[mask], adjmat.col[mask])), shape=adjmat.shape)
 
     def _compute_edge_index_sklearn(self, source_nodes: NodeStorage, target_nodes: NodeStorage) -> torch.Tensor:
+        source_coords, target_coords = self.get_cartesian_node_coordinates(source_nodes, target_nodes)
         nearest_neighbour = NearestNeighbors(metric="euclidean", n_jobs=4)
-        nearest_neighbour.fit(latlon_rad_to_cartesian(source_nodes.x).cpu())
+        nearest_neighbour.fit(source_coords.cpu())
         adj_matrix = nearest_neighbour.radius_neighbors_graph(
-            latlon_rad_to_cartesian(target_nodes.x).cpu(), radius=self.radius, mode="distance"
+            target_coords.cpu(), radius=self.radius, mode="distance"
         ).tocoo()
 
         adj_matrix = self._crop_to_max_num_neighbours(adj_matrix)

graphs/src/anemoi/graphs/edges/builders/knn.py
@@ -66,21 +66,23 @@ class KNNEdges(BaseEdgeBuilder, NodeMaskingMixin):
         assert num_nearest_neighbours > 0, "Number of nearest neighbours must be positive."
         self.num_nearest_neighbours = num_nearest_neighbours
 
+
+
     def _compute_edge_index_pyg(self, source_nodes: NodeStorage, target_nodes: NodeStorage) -> torch.Tensor:
         from torch_cluster.knn import knn
 
-        edge_index = knn(
-            latlon_rad_to_cartesian(source_nodes.x.to(self.device)),
-            latlon_rad_to_cartesian(target_nodes.x.to(self.device)),
-            k=self.num_nearest_neighbours,
-        )
+        source_coords, target_coords = self.get_cartesian_node_coordinates(source_nodes, target_nodes)
+
+        edge_index = knn(source_coords, target_coords, k=self.num_nearest_neighbours)
+
         return torch.flip(edge_index, [0])
 
     def _compute_edge_index_sklearn(self, source_nodes: NodeStorage, target_nodes: NodeStorage) -> torch.Tensor:
+        source_coords, target_coords = self.get_cartesian_node_coordinates(source_nodes, target_nodes)
         nearest_neighbour = NearestNeighbors(metric="euclidean", n_jobs=4)
-        nearest_neighbour.fit(latlon_rad_to_cartesian(source_nodes.x).cpu())
+        nearest_neighbour.fit(source_coords.cpu())
         adj_matrix = nearest_neighbour.kneighbors_graph(
-            latlon_rad_to_cartesian(target_nodes.x).cpu(),
+            target_coords.cpu(),
             n_neighbors=self.num_nearest_neighbours,
         ).tocoo()
 

graphs/src/anemoi/graphs/edges/builders/masking.py
@@ -12,8 +12,10 @@ from __future__ import annotations
 import logging
 
 import numpy as np
+import torch
 from scipy.sparse import coo_matrix
 from torch_geometric.data.storage import NodeStorage
+from anemoi.graphs.generate.transforms import latlon_rad_to_cartesian
 
 LOGGER = logging.getLogger(__name__)
 
@@ -23,9 +25,9 @@ class NodeMaskingMixin:
 
     def get_node_coordinates(
         self, source_nodes: NodeStorage, target_nodes: NodeStorage
-    ) -> tuple[np.ndarray, np.ndarray]:
+    ) -> tuple[torch.Tensor, torch.Tensor]:
         """Get the node coordinates."""
-        source_coords, target_coords = source_nodes.x.numpy(), target_nodes.x.numpy()
+        source_coords, target_coords = source_nodes.x, target_nodes.x
 
         if self.source_mask_attr_name is not None:
             source_coords = source_coords[source_nodes[self.source_mask_attr_name].squeeze()]
@@ -33,6 +35,14 @@ class NodeMaskingMixin:
         if self.target_mask_attr_name is not None:
             target_coords = target_coords[target_nodes[self.target_mask_attr_name].squeeze()]
 
+        return source_coords.to(self.device), target_coords.to(self.device)
+
+    def get_cartesian_node_coordinates(
+        self, source_nodes: NodeStorage, target_nodes: NodeStorage
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        source_coords, target_coords = self.get_node_coordinates(source_nodes, target_nodes)
+        source_coords = latlon_rad_to_cartesian(source_coords)
+        target_coords = latlon_rad_to_cartesian(target_coords)
         return source_coords, target_coords
 
     def undo_masking(self, adj_matrix, source_nodes: NodeStorage, target_nodes: NodeStorage):