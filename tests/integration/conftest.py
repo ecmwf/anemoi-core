fe3dac834b95ce3f44831991deef4f1b04105c2f
theissenhelen <helen.theissen@ecmwf.int>
2025-03-05 11:36:13 +0000

    test: use repo configs as templates for tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 from hydra import compose
 from hydra import initialize
@@ -17,12 +19,12 @@ from omegaconf import OmegaConf
 )
 def architecture_config(request) -> None:
     overrides = request.param
-    with initialize(version_base=None, config_path="", job_name="test_basic"):
+    with initialize(version_base=None, config_path="../../training/src/anemoi/training/config", job_name="test_basic"):
         template = compose(
-            config_name="config", overrides=overrides
+            config_name="debug", overrides=overrides
         )  # apply architecture overrides to template since they override a default
-        global_modifications = compose(config_name="test_training_cycle")
-        specific_modifications = compose(config_name="test_basic")
+        global_modifications = OmegaConf.load(Path.cwd() / "tests/integration/test_training_cycle.yaml")
+        specific_modifications = OmegaConf.load(Path.cwd() / "tests/integration/test_basic.yaml")
         cfg = OmegaConf.merge(template, global_modifications, specific_modifications)
         OmegaConf.resolve(cfg)
         return cfg
@@ -32,8 +34,8 @@ def architecture_config(request) -> None:
 def stretched_config() -> None:
     with initialize(version_base=None, config_path="", job_name="test_stretched"):
         template = compose(config_name="stretched")
-        global_modifications = compose(config_name="test_training_cycle")
-        specific_modifications = compose(config_name="test_stretched")
+        global_modifications = OmegaConf.load(Path.cwd() / "tests/integration/test_training_cycle.yaml")
+        specific_modifications = OmegaConf.load(Path.cwd() / "tests/integration/test_stretched.yaml")
         cfg = OmegaConf.merge(template, global_modifications, specific_modifications)
         OmegaConf.resolve(cfg)
         return cfg
