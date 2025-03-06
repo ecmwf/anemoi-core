ff3e231df2cdf7cbafe692f82715199b0134c6f1
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2025-03-06 11:10:33 +0000

    simplify

--- graphs/src/anemoi/graphs/nodes/builders/base.py
+++ graphs/src/anemoi/graphs/nodes/builders/base.py
@@ -140,11 +140,8 @@ class BaseNodeBuilder(ABC):
         t1 = time.time()
         LOGGER.debug("Time to register node coordinates (%s): %.2f s", self.__class__.__name__, t1 - t0)
 
-        if attrs_config is None:
-            attrs_config = {}  # We need to register the hidden attributes
-
         t0 = time.time()
-        graph = self.register_attributes(graph, attrs_config)
+        graph = self.register_attributes(graph, attrs_config or {})
         t1 = time.time()
         LOGGER.debug("Time to register node coordinates (%s): %.2f s", self.__class__.__name__, t1 - t0)
 
