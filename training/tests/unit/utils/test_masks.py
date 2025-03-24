b12cf45320c553b9bc0f35045cbd48cc81665e2f
dietervdb-meteo <dieter.vandenbleeken@meteo.be>
2025-03-24 17:30:18 +0200

    add test

---
+++ training/tests/unit/utils/test_masks.py
@@ -0,0 +1,35 @@
+# (C) Copyright 2024 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+import torch
+from anemoi.training.utils.masks import Boolean1DMask
+
+def test_apply_boolean1dmask(n=10, m=4) -> None:
+    """Test Boolean1DMask(mask).apply()"""
+    mask = torch.tensor([ i < m for i in range(n)])
+    x = torch.tensor([ -1 for i in range(n)],dtype=torch.float32)
+    fill_value = torch.tensor([ i for i in range(n)], dtype=torch.float32)
+    a = torch.rand(2,3)
+    b = torch.rand(3,2)
+    c = torch.rand(2,3)
+    d = torch.rand(3,2)
+    x = torch.tensordot(a, x, dims=0)
+    x = torch.tensordot(x, b, dims=0)
+    fill_value = torch.tensordot(c, fill_value, dims=0)
+    fill_value = torch.tensordot(fill_value, d, dims=0)
+    y = Boolean1DMask(mask).apply(x, dim = 2, fill_value = fill_value, dim_sel = 2)
+    expected_y0 = torch.tensor([ -1 for i in range(m)], dtype=torch.float32)
+    expected_y0 = torch.tensordot(a, expected_y0, dims=0)
+    expected_y0 = torch.tensordot(expected_y0, b, dims=0)
+    expected_y1 = torch.tensor([ i for i in range(m, n)], dtype=torch.float32)
+    expected_y1 = torch.tensordot(c, expected_y1, dims=0)
+    expected_y1 = torch.tensordot(expected_y1, d, dims=0)
+    expected_y = torch.cat((expected_y0, expected_y1), dim=2)
+    assert torch.equal(y, expected_y)
+    
