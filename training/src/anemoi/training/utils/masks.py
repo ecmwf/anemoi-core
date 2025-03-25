9489a902fc11a91ef5bbd2844ccbe966eed855b9
dietervdb-meteo <dieter.vandenbleeken@meteo.be>
2025-03-25 15:03:49 +0200

    move indices to correct device

--- training/src/anemoi/training/utils/masks.py
+++ training/src/anemoi/training/utils/masks.py
@@ -97,6 +97,7 @@ class Boolean1DMask(BaseMask):
         """
         if isinstance(fill_value, torch.Tensor):
             indices = (~self.mask).nonzero(as_tuple=True)[0]
+            indices = indices.to(x.get_device())
             return Boolean1DMask._fill_tensor_with_tensor(x, indices, fill_value, dim)
 
         mask = self.broadcast_like(x, dim)
