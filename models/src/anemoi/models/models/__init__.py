42d4de0415ef5cd37a662aecc3ef8f14c7061619
icedoom888 <alberto.pennino.8@gmail.com>
2025-04-09 17:39:07 +0200

    Merge main

--- models/src/anemoi/models/models/__init__.py
+++ models/src/anemoi/models/models/__init__.py
@@ -18,6 +18,7 @@ __all__ = [
     "AnemoiModelEncProcDec",
     "AnemoiModelEncProcDecHierarchical",
     "AnemoiModelEncProcDecInterpolator",
+    "AnemoiEnsModelEncProcDec",
     "AnemoiModelAutoEncoder",
     "AnemoiModelHierarchicalAutoEncoder",
 ]
