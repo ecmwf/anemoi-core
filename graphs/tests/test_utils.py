bc06e6df57a1d05bbd0bad30966942198b38d8bf
dietervdb-meteo <dieter.vandenbleeken@meteo.be>
2025-04-14 14:53:08 +0300

    add test get_edge_attributes

--- graphs/tests/test_utils.py
+++ graphs/tests/test_utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 
 from anemoi.graphs.utils import concat_edges
+from anemoi.graphs.utils import get_edge_attributes
 
 
 def test_concat_edges():
@@ -16,3 +17,26 @@ def test_concat_edges():
 
     assert torch.allclose(result1, expected1)
     assert torch.allclose(result2, edge_indices2)
+
+def test_get_edge_attributes():
+    mock_config = {
+        "nodes": "mock_nodes_config",
+        "edges": [
+            {
+                "source_name": "mock_nodes",
+                "target_name": "mock_nodes",
+                "attributes" : {'attr': 'attr_config'},
+                "extra_keys": "extra_values",
+            },
+        ],
+    }
+    edge_attrs1=get_edge_attributes(mock_config, "mock_nodes", "mock_nodes")
+    edge_attrs2=get_edge_attributes(mock_config, "mock_nodes", "other_nodes")
+    edge_attrs3=get_edge_attributes(mock_config, "other_nodes", "mock_nodes")
+
+    expected1=mock_config["edges"]["attributes"]
+
+    assert edge_attrs1 == expected1
+    assert edge_attrs2 == {}
+    assert edge_attrs3 == {}
+
