dc80e22e480709e0e40fc16af7311f73143af77f
Marek Jacob <1129-b380572@users.noreply.gitlab.dkrz.de>
2025-04-24 15:37:07 +0200

    Update copy right header

--- training/tests/integration/aicon/test_cicd_aicon_04_icon-dream_medium.py
+++ training/tests/integration/aicon/test_cicd_aicon_04_icon-dream_medium.py
@@ -1,10 +1,11 @@
-# (C) Copyright 2025 European Centre for Medium-Range Weather Forecasts.
+# (C) Copyright 2025 Anemoi contributors.
+#
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
 # In applying this licence, ECMWF does not waive the privileges and immunities
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
-#
 #
 # Various tests of the Anemoi components using a sample data set.
 #
