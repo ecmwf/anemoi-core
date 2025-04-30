a6e437caaecda0faedbdc0398db7c967546f1729
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2025-04-30 09:48:17 +0000

    add test for current issue

---
+++ graphs/tests/edges/test_direction.py
@@ -0,0 +1,23 @@
+import torch
+
+from anemoi.graphs.edges.directional import direction_vec
+
+
+def test_direction_vec():
+    points = torch.tensor(
+        [
+            [0, 0, 1],
+            [0, 0, -1],
+            [0, 1, 0],
+            [0, -1, 0],
+            [1, 0, 0],
+            [-1, 0, 0],
+            [1, 1, 0],
+            [-1, -1, 0],
+        ],
+        dtype=torch.float32,
+    )
+    direction = direction_vec(points, torch.tensor([[0, 0, 1]], dtype=torch.float32))
+    assert direction.shape == (points.shape[0], 3)
+    assert torch.all(direction[:, -1] == 0)
+    assert torch.all(torch.norm(direction, dim=1) == 1)
