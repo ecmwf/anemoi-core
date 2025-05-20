595c996c66e196ff1f98ea3bde346ea4782341ce
Magnus Sikora Ingstad <magnusi@met.no>
2025-05-20 13:49:38 +0200

    fix: add delayed scaler init to interpolator

--- training/src/anemoi/training/train/forecaster/interpolator.py
+++ training/src/anemoi/training/train/forecaster/interpolator.py
@@ -96,7 +96,13 @@ class GraphInterpolator(GraphForecaster):
         metrics = {}
         y_preds = []
 
+        # for validation not normalized in-place because remappers cannot be applied in-place
         batch = self.model.pre_processors(batch, in_place=not validation_mode)
+
+        # Delayed scalers need to be initialized after the pre-processors once
+        if self.is_first_step:
+            self.define_delayed_scalers()
+            self.is_first_step = False
 
         x_bound = batch[:, itemgetter(*self.boundary_times)(self.imap)][
             ...,
