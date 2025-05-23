6c337088776aa65d2c5bd4ed1789722ff60a6445
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-05-23 11:37:01 +0200

    use key instead of name

--- graphs/tests/nodes/test_from_xarray.py
+++ graphs/tests/nodes/test_from_xarray.py
@@ -4,7 +4,7 @@ from torch_geometric.data import HeteroData
 from anemoi.graphs.nodes.builders.from_file import XArrayNodes
 
 
-def test_can_load(mock_zarr_dataset_file):
+def test_update_graph_creates_expected_nodes(mock_zarr_dataset_file):
     graph = HeteroData()
 
     xarray_nodes = XArrayNodes(
@@ -18,13 +18,21 @@ def test_can_load(mock_zarr_dataset_file):
     assert graph["xarray_nodes"].num_nodes == 25
 
 
-def test_throws_error_with_invalid_lat(mock_zarr_dataset_file):
+@pytest.mark.parametrize(
+    "lat_key, lon_key",
+    [
+        ("invalid_latitude", "longitude"),
+        ("latitude", "invalid_longitude"),
+    ],
+)
+def test_throws_error_with_invalid_lat(mock_zarr_dataset_file, lat_key, lon_key):
     graph = HeteroData()
 
     xarray_nodes = XArrayNodes(
         dataset=mock_zarr_dataset_file,
         name="xarray_nodes",
-        lat_name="invalid_latitude",
+        lat_key=lat_key,
+        lon_key=lon_key,
     )
 
     with pytest.raises(AssertionError):
