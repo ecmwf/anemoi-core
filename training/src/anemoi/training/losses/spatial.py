e4c3a26dba419b9d6af0267728dc992b689fc935
omiralles <ophelia.miralles@meteoswiss.ch>
2025-06-20 09:59:29 +0200

    From review

--- training/src/anemoi/training/losses/spatial.py
+++ training/src/anemoi/training/losses/spatial.py
@@ -26,41 +26,43 @@ def amplitude(spectrum: torch.Tensor) -> torch.Tensor:
 
 def get_power_spectra_scalar_product(
     power_spectra_real: torch.Tensor,
-    power_spectra_fake: torch.Tensor,
+    power_spectra_pred: torch.Tensor,
 ) -> torch.Tensor:
-    return power_spectra_real * power_spectra_fake.conj() + power_spectra_fake * power_spectra_real.conj()
+    return power_spectra_real * power_spectra_pred.conj() + power_spectra_pred * power_spectra_real.conj()
 
 
 def get_spectra(
-    fake_output: torch.Tensor,
+    predicted_output: torch.Tensor,
     real_output: torch.Tensor,
     dims: tuple[int, int],
 ) -> tuple[torch.Tensor, torch.Tensor]:
-    assert dims[0] * dims[1] == real_output.shape[2]
+    assert dims[0] * dims[1] == real_output.shape[2], ("The product of dims must match the spatial dims of the output."
+    "Please use x_dim and y_dim such that field_shape=(x_dim, y_dim).")
     dims_total = (*real_output.shape[:2], *dims, real_output.shape[-1])
     power_spectra_real = torch.fft.rfft2(real_output.reshape(dims_total), dim=(-2, -3))
-    power_spectra_fake = torch.fft.rfft2(fake_output.reshape(dims_total), dim=(-2, -3))
-    return power_spectra_real, power_spectra_fake
+    power_spectra_pred = torch.fft.rfft2(predicted_output.reshape(dims_total), dim=(-2, -3))
+    return power_spectra_real, power_spectra_pred
 
 
-def log_rfft2_distance(fake_output: torch.Tensor, real_output: torch.Tensor, dims: tuple[int, int]) -> torch.Tensor:
-    power_spectra_real, power_spectra_fake = get_spectra(fake_output, real_output, dims)
+def log_rfft2_distance(predicted_output: torch.Tensor, real_output: torch.Tensor, dims: tuple[int, int]) -> torch.Tensor:
+    r"""Calculate the log spectral distance between two fields."""
+    power_spectra_real, power_spectra_pred = get_spectra(predicted_output, real_output, dims)
     epsilon = torch.finfo(real_output.dtype).eps  # Small epsilon to avoid division by zero
     power_spectra_real_sq = amplitude(power_spectra_real) ** 2
-    power_spectra_fake_sq = amplitude(power_spectra_fake) ** 2
-    ratio = (power_spectra_real_sq + epsilon) / (power_spectra_fake_sq + epsilon)
+    power_spectra_pred_sq = amplitude(power_spectra_pred) ** 2
+    ratio = (power_spectra_real_sq + epsilon) / (power_spectra_pred_sq + epsilon)
 
     def log10(x: torch.Tensor) -> torch.Tensor:
         return torch.log(x) / torch.log(torch.tensor(10.0, device=x.device, dtype=x.dtype))
 
     log_10 = (10 * log10(ratio)) ** 2
-    return torch.sqrt(torch.mean(log_10)) / 10
+    return log_10
 
 
 class LogFFT2Distance(FunctionalLoss):
     r"""The log spectral distance is used to compute the difference between spectra of two fields.
 
-    It is also called log spectral distorsion.
+    It is also called log spectral distortion.
     When it is expressed in discrete space with L2 norm, it is defined as:
     <math>D_{LS}={\left\{ \frac{1}{N} \sum_{n=1}^N \left[ \log P(n) - \log\hat{P}(n)\right]^2\right\\}}^{1/2} ,</math>.
     All scaling and weighting is handled by the parent class.
@@ -78,7 +80,6 @@ class LogFFT2Distance(FunctionalLoss):
         )
         self.x_dim = x_dim
         self.y_dim = y_dim
-        LOGGER.warning("Please use x_dim and y_dim such that field_shape=(x_dim, y_dim).")
 
     def calculate_difference(self, pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         return log_rfft2_distance(pred, target, dims=(self.x_dim, self.y_dim))
@@ -114,13 +115,12 @@ class FourierCorrelationLoss(FunctionalLoss):
         )
         self.x_dim = x_dim
         self.y_dim = y_dim
-        LOGGER.warning("Please use x_dim and y_dim such that field_shape=(x_dim, y_dim).")
 
     def calculate_difference(self, pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-        power_spectra_real, power_spectra_fake = get_spectra(pred, target, dims=(self.x_dim, self.y_dim))
+        power_spectra_real, power_spectra_pred = get_spectra(pred, target, dims=(self.x_dim, self.y_dim))
         self.power_spectra_real = power_spectra_real
-        self.power_spectra_fake = power_spectra_fake
-        return get_power_spectra_scalar_product(power_spectra_real, power_spectra_fake).real
+        self.power_spectra_pred = power_spectra_pred
+        return get_power_spectra_scalar_product(power_spectra_real, power_spectra_pred).real
 
     def forward(
         self,
@@ -149,7 +149,7 @@ class FourierCorrelationLoss(FunctionalLoss):
         epsilon = torch.finfo(target.dtype).eps  # Small epsilon to avoid division by zero
         denominator = torch.sqrt(
             torch.sum(amplitude(self.power_spectra_real) ** 2, dim=(-2, -3))
-            * torch.sum(amplitude(self.power_spectra_fake) ** 2, dim=(-2, -3))
+            * torch.sum(amplitude(self.power_spectra_pred) ** 2, dim=(-2, -3))
             + epsilon,
         )
         return torch.mean(1 - numerator / denominator)
