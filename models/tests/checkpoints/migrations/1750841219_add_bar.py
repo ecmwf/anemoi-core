0b4b810c3adebea38162700ba492f82eca48507c
devillerb <benjamin.devillers@meteo.fr>
2025-06-25 16:48:19 +0200

    add rollbacks to specific target

--- models/tests/checkpoints/migrations/1750841219_add_bar.py
+++ models/tests/checkpoints/migrations/1750841219_add_bar.py
@@ -1,20 +1,30 @@
+# (C) Copyright 2024 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
 from anemoi.models.migrations import CkptType
 from anemoi.models.migrations import Versions
 
 versions: Versions = {
     "migration": "1.0.0",
-    "anemoi-models": "0.8.1.post1",
+    "anemoi-models": "0.8.1",
 }
 
 
-def upgrade(ckpt: CkptType) -> CkptType:
-    """Migrate the model"""
+def migrate(ckpt: CkptType) -> CkptType:
+    """Migrate the checkpoint"""
     assert "bar" not in ckpt
     ckpt["bar"] = "bar"
     return ckpt
 
 
-def downgrade(ckpt: CkptType) -> CkptType:
-    """Cancels the upgrade function"""
+def rollback(ckpt: CkptType) -> CkptType:
+    """Rollbacks the migration"""
     del ckpt["bar"]
     return ckpt
