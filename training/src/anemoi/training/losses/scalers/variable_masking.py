121dbf03bc423980cd514f58af5b9368973c0375
Ophélia Miralles <opheliamiralles@gmail.com>
2025-07-11 11:27:43 +0200

    Update training/src/anemoi/training/losses/scalers/variable_masking.py
    Co-authored-by: Harrison Cook <Harrison.cook@ecmwf.int>

--- training/src/anemoi/training/losses/scalers/variable_masking.py
+++ training/src/anemoi/training/losses/scalers/variable_masking.py
@@ -26,17 +26,21 @@ LOGGER = logging.getLogger(__name__)
 
 
 class VariableMaskingLossScaler(GeneralVariableLossScaler):
-    """Class for masking variables in the loss."""
+    """Class for masking variables out in the loss.
+    
+    Use `invert=True` to set this to only mask the specified
+    variables in.
+    """
 
     def __init__(
         self,
         variables: list[str],
         data_indices: IndexCollection,
         metadata_extractor: ExtractVariableGroupAndLevel,
-        inverse: bool = False,
+        invert: bool = False,
         norm: str | None = None,
         **kwargs,
     ) -> None:
-        weights = dict.fromkeys(variables, 0.0) if not inverse else dict.fromkeys(variables, 1.0)
-        weights["default"] = 1.0 if not inverse else 0.0
+        weights = dict.fromkeys(variables, 0.0) if not invert else dict.fromkeys(variables, 1.0)
+        weights["default"] = 1.0 if not invert else 0.0
         super().__init__(data_indices, DictConfig(weights), metadata_extractor, norm, **kwargs)
