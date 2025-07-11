fa7c912c4147085520b1dc4ece92c67b924b8c84
icedoom888 <alberto.pennino.8@gmail.com>
2025-07-11 13:03:47 +0200

    fixed paths in import * adnace_input in ensforecaster

--- training/src/anemoi/training/train/tasks/interpolator.py
+++ training/src/anemoi/training/train/tasks/interpolator.py
@@ -18,8 +18,7 @@ from torch.utils.checkpoint import checkpoint
 from torch_geometric.data import HeteroData
 
 from anemoi.models.data_indices.collection import IndexCollection
-
-from .base import BaseGraphModule
+from anemoi.training.train.tasks.base import BaseGraphModule
 
 LOGGER = logging.getLogger(__name__)
 
