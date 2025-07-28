6f23319bbe41c94c9021685e7e8ad9cf4c7b3555
Cathal OBrien <cathal.obrien@ecmwf.int>
2025-07-28 12:42:13 +0000

    merge wip

--- training/tests/integration/conftest.py
+++ training/tests/integration/conftest.py
@@ -194,21 +194,25 @@ def gnn_config(
 
     cfg = OmegaConf.merge(template, testing_modifications_with_temp_dir, use_case_modifications)
     OmegaConf.resolve(cfg)
-<<<<<<< HEAD
     return cfg
 
 @pytest.fixture
 def benchmark_config_with_data(
     testing_modifications_with_temp_dir: OmegaConf,
-) -> OmegaConf:
+    get_tmp_paths: callable,
+) -> tuple[OmegaConf, str]:
 
     with initialize(version_base=None, config_path="../../src/anemoi/training/config", job_name="benchmakr"):
         template = compose(config_name="config")
 
     use_case_modifications = OmegaConf.load(Path.cwd() / "training/tests/integration/config/benchmark.yaml")
-    tmp_dir, rel_paths = _download_datasets(use_case_modifications, ["dataset"])
-=======
-    return cfg, dataset_urls[0]
+    tmp_dir, rel_paths, dataset_urls =  get_tmp_paths(use_case_modifications, ["dataset"])
+    use_case_modifications.hardware.paths.data = tmp_dir
+    use_case_modifications.hardware.files.dataset = rel_paths[0]
+
+    cfg = OmegaConf.merge(template, testing_modifications_with_temp_dir, use_case_modifications)
+    OmegaConf.resolve(cfg)
+    return cfg
 
 
 @pytest.fixture
