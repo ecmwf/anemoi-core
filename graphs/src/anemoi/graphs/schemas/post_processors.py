2072d55ed660a5e67c44645046344558b87d4169
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2025-07-31 11:45:11 +0000

    update type hints

--- graphs/src/anemoi/graphs/schemas/post_processors.py
+++ graphs/src/anemoi/graphs/schemas/post_processors.py
@@ -23,7 +23,7 @@ LOGGER = logging.getLogger(__name__)
 class RemoveUnconnectedNodesSchema(BaseModel):
     target_: Literal["anemoi.graphs.processors.RemoveUnconnectedNodes"] = Field(..., alias="_target_")
     "Post processor to remove unconnected nodes."
-    nodes_name: Union[str, Iterable[str]]
+    nodes_name: str | Iterable[str]
     "Nodes from which to remove the unconnected nodes."
     ignore: str = Field(example=None)
     "Attribute name of nodes to be ignored."
@@ -34,11 +34,11 @@ class RemoveUnconnectedNodesSchema(BaseModel):
 class SubsetNodesInAreaSchema(BaseModel):
     target_: Literal["anemoi.graphs.processors.SubsetNodesInArea"] = Field(..., alias="_target_")
     "Post processor to remove unconnected nodes."
-    nodes_name: Union[str, Iterable[str]]
+    nodes_name: str | Iterable[str]
     "Nodes from which to remove the unconnected nodes."
     area: tuple[float, float, float, float] = Field(default=(40, 10, 30, 20))
     "Area of interest to crop the nodes, (north, west, south, east)."
-    nodes_name: Union[str, Iterable[str]]
+    nodes_name: str | Iterable[str]
     "Nodes from which to remove the unconnected nodes."
     save_mask_indices_to_attr: str = Field(example=None)
     "New attribute name to store the mask indices."
