bf1abcc02db3a86d17ff19b036701f8b98132df5
Michiel Van Ginderachter <michiel.vanginderachter@meteo.be>
2025-08-08 12:51:35 +0300

    update RestrictEdgeLength schema

--- graphs/src/anemoi/graphs/schemas/post_processors.py
+++ graphs/src/anemoi/graphs/schemas/post_processors.py
@@ -39,12 +39,11 @@ class RestrictEdgeLengthSchema(BaseModel):
     "Target nodes of edges to be post-processed."
     max_length_km: float
     "Treshold length (in km), edges longer than this length will be removed"
-    source_mask_attr_name: str = Field(example=None)
+    source_mask_attr_name: str | None = Field(default=None, example=None)
     "Boolean mask attribute on sources nodes. Only edges whose source is True under this mask will be post-processed"
-    target_mask_attr_name: str = Field(example=None)
+    target_mask_attr_name: str | None = Field(default=None, example=None)
     "Boolean mask attribute on target nodes. Only edges whose target is True under this mask will be post-processed"
-    update_attributes: dict = Field(example=None)
-    "Configuration of edge attributes to be (re)computed."
+
 
 
 class SortEdgeIndexSchema(BaseModel):
