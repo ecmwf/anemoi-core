39605d6b56a984109309d5f189f86af4c190da11
Cathal OBrien <cathal.obrien@ecmwf.int>
2025-08-14 07:35:48 +0000

    added note to explain differences between attention implementations

--- models/src/anemoi/models/layers/attention.py
+++ models/src/anemoi/models/layers/attention.py
@@ -35,6 +35,24 @@ class MultiHeadSelfAttention(nn.Module):
     allows for three different attention implementations:
     - scaled dot product attention, see https://pytorch.org/docs/stable/generated/torch.nn.functional.scaled_dot_product_attention.html
     - flash attention, see https://github.com/Dao-AILab/flash-attention
+    - flash attention v3, see https://github.com/Dao-AILab/flash-attention#flashattention-3-beta-release
+
+    scaled dot product attention is a pytorch function, so it is easiest to use but the least performant.
+        It runs on CPUs and GPUs.
+
+    flash attention is optimised for efficient usage of the GPUs memory hierarchy. It loads smaller chunks
+        into fast local memory, and fuses attention into a single kernel to reduce the passes through memory.
+        It runs on Nvidia Ampere (e.g. A100) GPUs or newer and AMD MI200 GPUs or newer. Check the GitHub for
+        the full requirements.
+        You have to install flash attention yourself. If you are running on an x86 system, there are prebuilt
+        wheels available on the GitHub. On an aarch64 system, you have to build flash attention from source.
+
+    flash attention v3 uses new GPU features to achieve an up to 2x speedup compared to flash attention v2.
+        The new features include more efficient tensor core usage via the WGMMA instruction and hardware
+        accelerated memory accesses via the Tensor Memory Accelerator.
+        It requires Nvidia Hopper (e.g. H100) or newer GPUs. Check the Github for the full requirements.
+        You have to install flash attention v3 yourself. There are currently no pre-built wheels, so it must
+        be built from source.
     """
 
     def __init__(
@@ -339,8 +357,6 @@ class FlashAttentionV3Wrapper(nn.Module):
             import flash_attn_interface
         except ImportError as e:
             raise ImportError(f"Error importing flash-attn v3\n{e}")
-        if version.parse(flash_attn_interface.__version__) < version.parse("3.0.0"):
-            raise RuntimeError("Error: Flash-attn version is too low. Update to 3.0.0 or higher to use flash-attn v3.")
 
         self.attention = flash_attn_interface.flash_attn_func
 
