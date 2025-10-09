a10a53fece5a8341896b05a5477bca72e9cd186c
icedoom888 <alberto.pennino.8@gmail.com>
2025-10-09 14:25:01 +0200

    Fixed hierarchical auto

--- models/src/anemoi/models/models/autoencoder.py
+++ models/src/anemoi/models/models/autoencoder.py
@@ -23,6 +23,10 @@ from anemoi.models.distributed.graph import shard_tensor
 from anemoi.models.distributed.shapes import get_shard_shapes
 from anemoi.utils.config import DotDict
 
+from anemoi.models.layers.bounding import build_boundings
+from anemoi.models.layers.graph import NamedNodesAttributes
+from anemoi.models.layers.truncation import BaseTruncation
+
 from .base import BaseGraphModel
 
 LOGGER = logging.getLogger(__name__)
@@ -235,13 +239,40 @@ class AnemoiModelHierarchicalAutoEncoder(AnemoiModelAutoEncoder):
             Graph definition
         """
 
-        super().__init__(
-            model_config=model_config,
-            data_indices=data_indices,
-            statistics=statistics,
-            graph_data=graph_data,
-            truncation_data=truncation_data,
-        )
+        nn.Module.__init__(self)
+        self._graph_data = graph_data
+        self.data_indices = data_indices
+        self.statistics = statistics
+        self._truncation_data = truncation_data
+
+        model_config = DotDict(model_config)
+        self._graph_name_data = model_config.graph.data
+        self._graph_hidden_names = model_config.graph.hidden
+        self.num_hidden = len(self._graph_hidden_names)
+        self.multi_step = model_config.training.multistep_input
+        num_channels = model_config.model.num_channels
+
+        # hidden_dims is the dimentionality of features at each depth
+        self.hidden_dims = {hidden: num_channels * (2**i) for i, hidden in enumerate(self._graph_hidden_names)}
+
+        # Unpack config for hierarchical graph
+        self.level_process = model_config.model.enable_hierarchical_level_processing
+        self.node_attributes = NamedNodesAttributes(model_config.model.trainable_parameters.hidden, self._graph_data)
+
+        self._calculate_shapes_and_indices(data_indices)
+        self._assert_matching_indices(data_indices)
+
+        # build networks
+        self._build_networks(model_config)
+
+        # build truncation
+        self.truncation = BaseTruncation(self._truncation_data)
+
+        # build boundings
+        self.boundings = build_boundings(model_config, self.data_indices, self.statistics)
+    
+    def _calculate_input_dim_latent(self):
+        return self.node_attributes.attr_ndims[self._graph_hidden_names[0]]
 
     def _build_networks(self, model_config):
         # Encoder data -> hidden
