202fd554b7648b5245bdae0a8f8633a2acdc84ab
anaprietonem <ana.prietonemesio@ecmwf.int>
2025-10-21 14:01:42 +0000

    clean up profiler

--- training/tests/unit/diagnostics/test_callbacks.py
+++ training/tests/unit/diagnostics/test_callbacks.py
@@ -28,8 +28,6 @@ diagnostics:
     # this will detect and trace back NaNs / Infs etc. but will slow down training
     anomaly_detection: False
 
-  profiler: False
-
   enable_checkpointing: False
   checkpoint:
 
