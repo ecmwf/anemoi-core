2b905a7a0cdd96e794efcd81c6f8da7aa9e3b314
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2025-11-07 14:12:56 +0000

    advance_input moved to BaseRolloutForecaster

--- training/src/anemoi/training/train/tasks/ensforecaster.py
+++ training/src/anemoi/training/train/tasks/ensforecaster.py
@@ -179,37 +179,6 @@ class GraphEnsForecaster(BaseRolloutGraphModule):
 
         return loss_inc, y_pred_ens if return_pred_ens else None
 
-    def advance_input(
-        self,
-        x: torch.Tensor,
-        y_pred: torch.Tensor,
-        batch: torch.Tensor,
-        rollout_step: int,
-    ) -> torch.Tensor:
-        x = x.roll(-1, dims=1)
-
-        # Get prognostic variables
-        x[:, -1, :, :, self.data_indices.model.input.prognostic] = y_pred[
-            ...,
-            self.data_indices.model.output.prognostic,
-        ]
-
-        x[:, -1] = self.output_mask.rollout_boundary(
-            x[:, -1],
-            batch[:, self.multi_step + rollout_step],
-            self.data_indices,
-        )
-
-        # get new "constants" needed for time-varying fields
-        x[:, -1, :, :, self.data_indices.model.input.forcing] = batch[
-            :,
-            self.multi_step + rollout_step,
-            :,
-            :,
-            self.data_indices.data.input.forcing,
-        ]
-        return x
-
     def rollout_step(
         self,
         batch: torch.Tensor,
