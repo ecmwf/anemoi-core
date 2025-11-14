bef3c3a832796bcbc0b9e2c9a354b4c977e92eab
avritj <julien.avrit@meteo.fr>
2025-11-14 09:37:44 +0100

    first commit

--- graphs/src/anemoi/graphs/processors/__init__.py
+++ graphs/src/anemoi/graphs/processors/__init__.py
@@ -2,10 +2,11 @@ from .post_process import RemoveUnconnectedNodes
 from .post_process import RestrictEdgeLength
 from .post_process import SortEdgeIndexBySourceNodes
 from .post_process import SortEdgeIndexByTargetNodes
-
+from .post_process import SubsetNodesInArea
 __all__ = [
     "RemoveUnconnectedNodes",
     "RestrictEdgeLength",
     "SortEdgeIndexByTargetNodes",
     "SortEdgeIndexBySourceNodes",
+    "SubsetNodesInArea"
 ]
