0ec632ee111e0520695935f65910498ac79f9c34
Mario Santa Cruz <48736305+JPXKQX@users.noreply.github.com>
2025-11-20 09:30:27 +0000

    Update models/src/anemoi/models/layers/residual.py
    Co-authored-by: Harrison Cook <Harrison.cook@ecmwf.int>

--- models/src/anemoi/models/layers/residual.py
+++ models/src/anemoi/models/layers/residual.py
@@ -7,6 +7,7 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
+
 from abc import ABC
 from abc import abstractmethod
 from typing import Optional
