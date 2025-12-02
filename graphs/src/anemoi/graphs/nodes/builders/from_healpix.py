3f4f7c4f0a91ce260b40b77c805b8eb7d9fd222f
Mario Santa Cruz <48736305+JPXKQX@users.noreply.github.com>
2025-12-02 16:16:29 +0000

    Update from_healpix.py

--- graphs/src/anemoi/graphs/nodes/builders/from_healpix.py
+++ graphs/src/anemoi/graphs/nodes/builders/from_healpix.py
@@ -82,7 +82,7 @@ class LimitedAreaHEALPixNodes(HEALPixNodes):
         margin_radius_km: float = 100.0,
     ) -> None:
         super().__init__(resolution, name)
-        self.area_mask_sbuilder = KNNAreaMaskBuilder(reference_node_name, margin_radius_km, mask_attr_name)
+        self.area_mask_builder = KNNAreaMaskBuilder(reference_node_name, margin_radius_km, mask_attr_name)
 
     def register_nodes(self, graph: HeteroData) -> None:
         self.area_mask_builder.fit(graph)
