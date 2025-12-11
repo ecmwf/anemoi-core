e0815aab1afa57c6b01b23b3f10eaeeac1abb88b
avritj <julien.avrit@meteo.fr>
2025-12-11 10:21:24 +0100

    adding class to plot with uncond diffusion at the end of an epoch

---
+++ training/src/anemoi/training/config/slurm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -l
+#SBATCH -J DE371_diffusion
+#SBATCH -A p200177
+#SBATCH -N 1
+#SBATCH -G 4
+#SBATCH -p gpu
+#SBATCH --ntasks-per-node=4
+#SBATCH --time=24:00:00
+#SBATCH --qos=default
+
+
+export TORCH_DISTRIBUTED_DEBUG=INFO 
+export OMP_NUM_THREADS=4
+export CUDA_HOME=/usr/local/cuda-12.1
+export NVHPC_CUDA_HOME=/usr/local/cuda-12.1
+export CXX=g++ #the compiler for cpp extensions
+export CC=gcc  #the compiler to access the good cpp standard
+export NCCL_ASYNC_ERROR_HANDLING=1
+module load env/release/2023.1
+module load env/staging/2023.1
+module load Apptainer/1.2.4-GCCcore-12.3.0
+module load NVHPC
+module load GCC
+
+
+# Echo des commandes lancees
+set -x
+
+# Ensure the logs directory exists or create it if not
+# if [ ! -d "$LOG_DIR" ]; then
+#   mkdir "$LOG_DIR"
+# fi
+cd /home/users/u102751/code/anemoi/anemoi-core/training
+export HYDRA_FULL_ERROR=1
+
+srun bash -c "puv srun anemoi-training train --config-name=diffusion_test"
