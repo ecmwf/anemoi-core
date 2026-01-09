1256feb46230e6701dc8719943eff697c3a720d8
icedoom888 <alberto.pennino.8@gmail.com>
2026-01-09 10:02:22 +0100

    Added latlon to base

--- training/src/anemoi/training/train/tasks/base.py
+++ training/src/anemoi/training/train/tasks/base.py
@@ -176,7 +176,7 @@ class BaseGraphModule(pl.LightningModule, ABC):
         self.data_indices = data_indices
 
         self.latlons_data = graph_data[config.graph.data].x
-        
+
         self.save_hyperparameters()
 
         self.statistics_tendencies = statistics_tendencies
