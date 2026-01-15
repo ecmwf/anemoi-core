76e6652f6fc6ed7fb49d48f263c7e3d266fbdd5b
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2026-01-15 17:17:02 +0000

    fix: move config utility to anemoi/models

--- models/src/anemoi/models/utils/config.py
+++ models/src/anemoi/models/utils/config.py
@@ -14,8 +14,6 @@ from omegaconf import OmegaConf
 DEFAULT_DATASET_NAME = "data"
 
 
-# This function retrieves the configuration for multiple datasets, supporting both new and old config formats.
-# Its location in the codebase may be revisited in the near future.
 def get_multiple_datasets_config(config: DictConfig, default_dataset_name: str = DEFAULT_DATASET_NAME) -> dict:
     """Get multiple datasets configuration for old configs.
     Use /'data/' as the default dataset name.
@@ -26,42 +24,3 @@ def get_multiple_datasets_config(config: DictConfig, default_dataset_name: str =
         return config.datasets
 
     return OmegaConf.create({default_dataset_name: config})
-
-
-def broadcast_config_keys(dictionary: dict[str, int], **kwargs) -> dict[str, int]:
-    """Broadcasts values from the input dictionary to multiple keys based on the provided mapping.
-
-    Parameters
-    ----------
-    dictionary : dict[str, int]
-        Input dictionary containing values to be broadcasted
-    **kwargs : dict[str, str | list[str]]
-        Mapping of old keys to new keys for broadcasting. Each key in kwargs is an old key from the input dictionary,
-        and its value is a list of new keys.
-
-    Returns
-    -------
-    dict[str, int]
-        New dictionary with values broadcasted to the new keys based on the provided mapping.
-
-    Example
-    -------
-    >>> input_dict = {'num_params': 10}
-    >>> broadcast_config_keys(input_dict, num_params='dataset1_num_params') # Broadcast to a single new key
-    {'dataset1_num_params': 10}
-    >>> new_keys = ['dataset1_num_params', 'dataset2_num_params']
-    >>> broadcast_config_keys(input_dict, num_params=new_keys)  # Broadcast to multiple new keys
-    {'dataset1_num_params': 10, 'dataset2_num_params': 10}
-    """
-    new_num_params = {}
-    for old_key, new_keys in kwargs.items():
-        if isinstance(new_keys, str):
-            new_keys = [new_keys]
-
-        for new_key in new_keys:
-            if old_key not in dictionary:
-                raise KeyError(f"Key '{old_key}' not found in the input dictionary for broadcasting.")
-
-            new_num_params[new_key] = dictionary[old_key]
-
-    return new_num_params
