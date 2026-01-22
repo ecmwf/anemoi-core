7a89cc45d1a3144455015247be7b33f7efe126d5
MeluXina user <u101957@login03.meluxina.lxp.lu>
2026-01-22 11:00:56 +0100

    working mulitnode

--- training/src/anemoi/training/config/init_diffusion_incond_x0_test_gpu.sh
+++ training/src/anemoi/training/config/init_diffusion_incond_x0_test_gpu.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH -J DE371_diffusion
 #SBATCH -A p200177
-#SBATCH -N 1
+#SBATCH -N 2
 #SBATCH -p gpu
+#SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-node=4
-#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
-#SBATCH --time=48:00:00
-#SBATCH --qos=default
+#SBATCH --time=04:00:00
+#SBATCH --qos=short
 set -x
 module load env/staging/2024.1
 module load NVHPC
@@ -19,9 +19,12 @@ export HYDRA_FULL_ERROR=1
 export OMP_NUM_THREADS=4
 export NCCL_ASYNC_ERROR_HANDLING=1
 export PYTHONPATH=/home/users/u101957/code/anemoi-core/models/src:/home/users/u101957/code/anemoi-core/training/src:$PYTHONPATH
-cd /home/users/u102751/code/anemoi/anemoi-core/training/src/anemoi/training/config
-# reload ckpt :
+echo $CUDA_VISIBLE_DEVICES
+cd /home/users/u101957/code/anemoi-core/training/src/anemoi/training/config
+CUDA_VISIBLE_DEVICES=0,1,2,3
+srun puv run anemoi-training train --config-name=diffusion_incond 
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 srun --ntasks=1 \
-  puv run torchrun --standalone --nproc_per_node=4 \
-  anemoi-training train config-name=config_angelique
+#   puv run torchrun  \
+#   --nnodes=2 \
+#   --nproc_per_node=4 \
+#   -m anemoi.training train --config-name=diffusion_incond
