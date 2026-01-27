a133f958b3d0a9849b3d963682d6db22041759ce
mc4117 <mariana.clare@ecmwf.int>
2026-01-27 17:16:41 +0000

    subclassing multi out interpolator model

--- training/src/anemoi/training/schemas/training.py
+++ training/src/anemoi/training/schemas/training.py
@@ -468,11 +468,19 @@ class InterpolationSchema(BaseTrainingSchema):
     target_forcing: DatasetDict[TargetForcing]
     "Forcing parameters for target output times."
 
+class InterpolationMultiSchema(BaseTrainingSchema):
+    model_task: Literal["anemoi.training.train.tasks.GraphMultiOutInterpolator"] = Field(..., alias="model_task")
+    "Training objective."
+    explicit_times: ExplicitTimes
+    "Time indices for input and output."
+    target_forcing: None
+    "Forcing parameters not applied for multi-outputs."
 
 TrainingSchema = Annotated[
     ForecasterSchema
     | ForecasterEnsSchema
     | InterpolationSchema
+    | InterpolationMultiSchema
     | DiffusionForecasterSchema
     | DiffusionTendForecasterSchema,
     Discriminator("model_task"),
