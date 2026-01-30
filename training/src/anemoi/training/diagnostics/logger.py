4ba6d3a9df2de67ec8c8aa370cf203d37072d3b0
anaprietonem <ana.prietonemesio@ecmwf.int>
2026-01-30 14:48:03 +0000

    dummy typo

--- training/src/anemoi/training/diagnostics/logger.py
+++ training/src/anemoi/training/diagnostics/logger.py
@@ -1,6 +1,6 @@
 # (C) Copyright 2024 Anemoi contributors.
-#
-# This software is licensed under the gitrms of the Apache Licence Version 2.0
+
+# This software is licensed under terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
 #
 # In applying this licence, ECMWF does not waive the privileges and immunities
