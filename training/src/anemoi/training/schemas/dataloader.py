29e215264aa7a4f6e6218b00f684df6d865d9075
icedoom888 <alberto.pennino.8@gmail.com>
2026-02-16 10:21:42 +0100

    Added select

--- training/src/anemoi/training/schemas/dataloader.py
+++ training/src/anemoi/training/schemas/dataloader.py
@@ -69,6 +69,8 @@ class NativeDatasetSchema(PydanticBaseModel):
     "Temporal resolution, frequency must be >= to dataset frequency."
     drop: list | None = Field(default=None)
     "List of variables to drop from dataset"
+    select: list | None = Field(default=None)
+    "List of variables to select from dataset, if not provided all variables are selected."
 
 
 class TrajectorySchema(PydanticBaseModel):
