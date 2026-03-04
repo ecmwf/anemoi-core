9e7df3c3c2fdc95238fc230d9b4e8f35873793e3
Ophélia Miralles <opheliamiralles@gmail.com>
2026-03-04 11:48:45 +0000

    Ruff

--- training/src/anemoi/training/losses/index_space.py
+++ training/src/anemoi/training/losses/index_space.py
@@ -7,10 +7,10 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
-from enum import Enum
+from enum import StrEnum
 
 
-class IndexSpace(str, Enum):
+class IndexSpace(StrEnum):
     """Tensor index-space for variable resolution in loss wrappers."""
 
     MODEL_OUTPUT = "model_output"
