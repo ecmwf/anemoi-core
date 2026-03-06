64085c5b5268c9f751ff517636be74df385bab6c
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2026-03-06 13:59:44 +0000

    fix

--- models/src/anemoi/models/models/base.py
+++ models/src/anemoi/models/models/base.py
@@ -69,7 +69,7 @@ class BaseGraphModel(nn.Module):
         self.n_step_input = model_config.training.multistep_input
         self.n_step_output = model_config.training.multistep_output
         self.num_channels = model_config.model.num_channels
-        self.latent_skip = model_config.model.latent_skip
+        self.latent_skip = model_config.model.model.latent_skip
 
         self.node_attributes = torch.nn.ModuleDict()
         for dataset_name in self._graph_data.keys():
