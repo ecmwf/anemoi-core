f82b6b76c93b034035c6ffcd455c941b467e6c0c
Mathias Schreiner <matschreiner@gmail.com>
2026-04-07 16:15:20 +0200

    add returntype to runtime_artifacts

--- training/src/anemoi/training/train/train.py
+++ training/src/anemoi/training/train/train.py
@@ -30,6 +30,7 @@ from pytorch_lightning.utilities.rank_zero import rank_zero_only
 from torch_geometric.data import HeteroData
 
 from anemoi.models.utils.compile import mark_for_compilation
+from anemoi.models.utils.runtime_artifacts import RuntimeArtifacts
 from anemoi.training.data.datamodule import AnemoiDatasetsDataModule
 from anemoi.training.diagnostics.callbacks import get_callbacks
 from anemoi.training.diagnostics.logger import get_mlflow_logger
@@ -373,10 +374,8 @@ class AnemoiTrainer(ABC):
         return get_callbacks(self.config)
 
     @cached_property
-    def runtime_artifacts(self):
+    def runtime_artifacts(self) -> RuntimeArtifacts:
         """Package raw runtime data for the model builder."""
-        from anemoi.models.utils.runtime_artifacts import RuntimeArtifacts
-
         return RuntimeArtifacts(
             statistics=self.datamodule.statistics,
             statistics_tendencies=self.datamodule.statistics_tendencies,
