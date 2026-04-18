fb27b87594c4a95152194bf217ceb606d568d6da
TingLei-daprediction <leiting2002@gmail.com>
2026-04-18 20:57:00 +0000

    WIP

--- dd-verify-3hr_leadtime-1month-123hr_loss.sh
+++ dd-verify-3hr_leadtime-1month-123hr_loss.sh
@@ -23,10 +23,11 @@ cd /scratch3/NCEPDEV/fv3-cam/Ting.Lei/dr-anemoi-core/anemoi-core
 #run_id="7be32a16-2f83-479c-8370-1dc631dbbf81"
 #run_id="7be32a16-2f83-479c-8370-1dc631dbbf81"
 #run_id="58a03191-bccf-45c6-9b3c-19b314d1fbe7"
+#run_id="5cdef9b4-a3fa-4105-8a31-edfbe0cc0c79"
 run_id="5cdef9b4-a3fa-4105-8a31-edfbe0cc0c79"
 training/docs/user-guide/examples/run_rrfs_verify_export_1to3h.sh \
   /scratch3/NCEPDEV/fv3-cam/Ting.Lei/tlei-anemoi-training/checkpoint/${run_id}/inference-last.ckpt \
-  2024-05-05T00:00:00 2024-05-05T23:00:00 1h
+  2024-05-05T00:00:00 2024-05-30T23:00:00 1h
 
 
 
