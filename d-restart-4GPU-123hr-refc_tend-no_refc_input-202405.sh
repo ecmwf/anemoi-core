375ef1153d493d2edfc1894e0309bc4ed3a8fcb1
TingLei-daprediction <leiting2002@gmail.com>
2026-04-21 21:19:25 +0000

    WIP

---
+++ d-restart-4GPU-123hr-refc_tend-no_refc_input-202405.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH -A fv3-cam
+#SBATCH -J anemoi-train
+#SBATCH -p u1-h100
+#SBATCH -q gpuwf
+#SBATCH -N 2
+#SBATCH --ntasks-per-node=2
+#SBATCH --gpus-per-node=2
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=0
+#SBATCH -t 24:00:00
+#SBATCH -o restart-anemoi-gpu-123hr_refc_tend-no_refc_input-train.%j.out
+#SBATCH -e restart-anemoi-gpu-123hr_refc_tend-no_refc_input-train.%j.err
+
+set -euo pipefail
+
+source /scratch3/NCEPDEV/fv3-cam/Ting.Lei/dr-miniconda3/bin/activate anemoi-training-env-python3.12
+cd /scratch3/NCEPDEV/fv3-cam/Ting.Lei/dr-anemoi-core/anemoi-core
+
+export ANEMOI_BASE_SEED=12345
+export HYDRA_FULL_ERROR=1
+export NCCL_ASYNC_ERROR_HANDLING=1
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export TRITON_CACHE_DIR=/scratch3/NCEPDEV/fv3-cam/Ting.Lei/triton_cache/${SLURM_JOB_ID}
+mkdir -p "$TRITON_CACHE_DIR"
+RUN_ID=95f6db58-d330-481c-8b1c-0eb58f39c621
+CKPT=/scratch3/NCEPDEV/fv3-cam/Ting.Lei/tlei-anemoi-training/checkpoint/${RUN_ID}/last.ckpt
+echo "Resuming run_id=${RUN_ID}"
+echo "Checkpoint=${CKPT}"
+test -f "${CKPT}"
+
+srun --gpu-bind=closest \
+  --export=ALL,TRITON_CACHE_DIR=/scratch3/NCEPDEV/fv3-cam/Ting.Lei/triton_cache/${SLURM_JOB_ID}/${SLURM_PROCID} \
+  anemoi-training train \
+    training.run_id=${RUN_ID} \
+    training.max_epochs=1860 \
+    --config-path /scratch3/NCEPDEV/fv3-cam/Ting.Lei/dr-anemoi-core/anemoi-core/training/docs/user-guide/examples \
+    --config-name anemoi-training-rrfs-lam-neural-lam-static-forcing-202405-123h-refc-tendency-loss-no-refc-input \
+    system.hardware.num_gpus_per_node=2 \
+    system.hardware.num_nodes=2 \
+    system.hardware.num_gpus_per_model=1
