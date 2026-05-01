69540715fa5c6a495ef6c608a282934e9b8c0714
TingLei-daprediction <leiting2002@gmail.com>
2026-05-01 11:45:44 +0000

    WIP

--- dd-verify-3hr_leadtime-1month-123hr_refc_value_loss-no_refc_input-loss_table.sh
+++ dd-verify-3hr_leadtime-1month-123hr_refc_value_loss-no_refc_input-loss_table.sh
@@ -8,7 +8,7 @@
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=0
-#SBATCH -t 8:00:00
+#SBATCH -t 2:00:00
 #SBATCH -o anemoi-gpu-1mon-3hrleadtime_123hr_refc_value_loss-no_refc_input-loss_table.%j.out
 #SBATCH -e anemoi-gpu-1mon-3hrleadtime_123hr_refc_value_loss-no_refc_input-loss_table.%j.err
 
@@ -17,7 +17,7 @@ set -euo pipefail
 source /scratch3/NCEPDEV/fv3-cam/Ting.Lei/dr-miniconda3/bin/activate anemoi-training-env-python3.12
 cd /scratch3/NCEPDEV/fv3-cam/Ting.Lei/dr-anemoi-core/anemoi-core
 
-run_id="CHANGE_ME_REFC_VALUE_NO_REFC_INPUT_RUN_ID"
+run_id="a475a4e9-4f4f-40b1-9652-2798904a727c"
 training/docs/user-guide/examples/run_rrfs_verify_loss_table_1to3h_refc_value_no_refc_input.sh \
   /scratch3/NCEPDEV/fv3-cam/Ting.Lei/tlei-anemoi-training/checkpoint/${run_id}/inference-last.ckpt \
   2024-05-05T00:00:00 2024-05-30T23:00:00 1h
