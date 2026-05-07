d8be3edf608988ddab1997e38f1cce40da38373b
TingLei-daprediction <leiting2002@gmail.com>
2026-05-07 12:00:54 +0000

    WIP

--- dd-verify-1hr_leadtime-1month-1hr_refc_value_loss-no_refc_input-finer_graph_v1.sh
+++ dd-verify-1hr_leadtime-1month-1hr_refc_value_loss-no_refc_input-finer_graph_v1.sh
@@ -17,7 +17,7 @@ set -euo pipefail
 source /scratch3/NCEPDEV/fv3-cam/Ting.Lei/dr-miniconda3/bin/activate anemoi-training-env-python3.12
 cd /scratch3/NCEPDEV/fv3-cam/Ting.Lei/dr-anemoi-core/anemoi-core
 
-run_id="REPLACE_WITH_RUN_ID"
+run_id="dfe60ea5-25f4-4c32-957a-81b58d203c26"
 training/docs/user-guide/examples/run_rrfs_verify_export_1h_refc_value_no_refc_input_finer_graph_v1.sh \
   /scratch3/NCEPDEV/fv3-cam/Ting.Lei/tlei-anemoi-training/checkpoint/${run_id}/inference-last.ckpt \
   2024-05-05T00:00:00 2024-05-30T23:00:00 1h
