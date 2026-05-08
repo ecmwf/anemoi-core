e27527930c5477e948f044c627eda2081a317826
arams <arams@met.no>
2026-05-08 13:35:27 +0200

    fix(training): fix import

--- training/src/anemoi/training/data/multidomain.py
+++ training/src/anemoi/training/data/multidomain.py
@@ -18,7 +18,7 @@ import torch
 from anemoi.models.distributed.balanced_partition import get_balanced_partition_range
 from anemoi.models.distributed.balanced_partition import get_partition_range
 from anemoi.training.data.data_reader import BaseAnemoiReader
-from anemoi.training.data.dataset import AnemoiDataset
+from training.src.anemoi.training.data.anemoidataset import AnemoiDataset
 from anemoi.training.data.usable_indices import get_usable_indices
 from anemoi.training.utils.seeding import get_base_seed
 from anemoi.training.utils.time_indices import TimeIndices
