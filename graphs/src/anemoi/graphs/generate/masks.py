a725495dd89650c53bd7020aee05330eae631d1e
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2026-05-21 09:13:05 +0000

    remove old KNNAreaMaskBuilder

--- graphs/src/anemoi/graphs/generate/masks.py
+++ graphs/src/anemoi/graphs/generate/masks.py
@@ -25,7 +25,7 @@ LOGGER = logging.getLogger(__name__)
 TORCH_CLUSTER_AVAILABLE = find_spec("torch_cluster") is not None
 
 
-class RadiusAreaMaskBuilder:
+class AreaMaskBuilder:
     """Class to build a mask based on a radius search using the similarity of the Euclidean chord length between
     two points on the unit sphere and their great-circle distance.
 
@@ -241,89 +241,6 @@ class RadiusAreaMaskBuilder:
             )
             mask = counts > 0
 
-        t1 = time.time()
-        LOGGER.debug("Time to get mask from (%s): %.2f s", self.__class__.__name__, t1 - t0)
-
-        return mask
-
-
-class KNNAreaMaskBuilder:
-    """Class to build a mask based on distance to masked reference nodes using KNN.
-
-    Attributes
-    ----------
-    nearest_neighbour : NearestNeighbors
-        Nearest neighbour object to compute the KNN.
-    margin_radius_km : float
-        Maximum distance to the reference nodes to consider a node as valid, in kilometers. Defaults to 100 km.
-    reference_node_name : str
-        Name of the reference nodes in the graph to consider for the Area Mask.
-    mask_attr_name : str
-        Name of a node to attribute to mask the reference nodes, if desired. Defaults to consider all reference nodes.
-
-    Methods
-    -------
-    fit_coords(coords_rad: np.ndarray)
-        Fit the KNN model to the coordinates in radians.
-    fit(graph: HeteroData)
-        Fit the KNN model to the reference nodes.
-    get_mask(coords_rad: np.ndarray) -> np.ndarray
-        Get the mask for the nodes based on the distance to the reference nodes.
-    """
-
-    def __init__(self, reference_node_name: str, margin_radius_km: float = 100, mask_attr_name: str | None = None):
-        assert isinstance(margin_radius_km, (int, float)), "The margin radius must be a number."
-        assert margin_radius_km > 0, "The margin radius must be positive."
-
-        self.nearest_neighbour = NearestNeighbors(metric="haversine", n_jobs=4)
-        self.margin_radius_km = margin_radius_km
-        self.reference_node_name = reference_node_name
-        self.mask_attr_name = mask_attr_name
-
-    def get_reference_coords(self, graph: HeteroData) -> np.ndarray:
-        """Retrive coordinates from the reference nodes."""
-        assert (
-            self.reference_node_name in graph.node_types
-        ), f'Reference node "{self.reference_node_name}" not found in the graph.'
-
-        coords_rad = graph[self.reference_node_name].x
-        if self.mask_attr_name is not None:
-            assert (
-                self.mask_attr_name in graph[self.reference_node_name].node_attrs()
-            ), f'Mask attribute "{self.mask_attr_name}" not found in the reference nodes.'
-            mask = graph[self.reference_node_name][self.mask_attr_name].squeeze()
-            coords_rad = coords_rad[mask]
-        LOGGER.debug(f"The coords_rad tensor is on {coords_rad.device}")
-        return coords_rad.cpu().numpy()
-
-    def fit_coords(self, coords_rad: np.ndarray):
-        """Fit the KNN model to the coordinates in radians."""
-        self.nearest_neighbour.fit(coords_rad)
-
-    def fit(self, graph: HeteroData):
-        """Fit the KNN model to the nodes of interest."""
-        # Prepare string for logging
-        reference_mask_str = self.reference_node_name
-        if self.mask_attr_name is not None:
-            reference_mask_str += f" ({self.mask_attr_name})"
-
-        # Fit to the reference nodes
-        coords_rad = self.get_reference_coords(graph)
-        self.fit_coords(coords_rad)
-
-        LOGGER.info(
-            'Fitting %s with %d reference nodes from "%s".',
-            self.__class__.__name__,
-            len(coords_rad),
-            reference_mask_str,
-        )
-
-    def get_mask(self, coords_rad: np.ndarray) -> np.ndarray:
-        """Compute a mask based on the distance to the reference nodes."""
-        LOGGER.debug("Getting mask from KNNAreaMaskBuilder")
-        t0 = time.time()
-        neigh_dists, _ = self.nearest_neighbour.kneighbors(coords_rad, n_neighbors=1)
-        mask = neigh_dists[:, 0] * EARTH_RADIUS <= self.margin_radius_km
         t1 = time.time()
         LOGGER.debug("Time to get mask from (%s): %.2f s", self.__class__.__name__, t1 - t0)
 
