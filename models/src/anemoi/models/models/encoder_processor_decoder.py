acbee55b270992a3be4c5c1f9f6d72137b263377
Petr Dolezal <sirelkir@gmail.com>
2026-05-23 00:01:52 +0300

    fix: allow dict of trainable parameters (copied from main)

--- models/src/anemoi/models/models/encoder_processor_decoder.py
+++ models/src/anemoi/models/models/encoder_processor_decoder.py
@@ -69,7 +69,7 @@ class AnemoiModelEncProcDec(nn.Module):
         self.multi_step = model_config.training.multistep_input
         self.num_channels = model_config.model.num_channels
 
-        self.node_attributes = NamedNodesAttributes(model_config.model.trainable_parameters.hidden, self._graph_data)
+        self.node_attributes = NamedNodesAttributes(model_config.model.trainable_parameters, self._graph_data)
 
         self._calculate_shapes_and_indices(data_indices)
         self._assert_matching_indices(data_indices)
