2e45cc4e4bba48d263045139d0574d484eec4cd7
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2026-06-02 06:27:14 +0000

    fix: checkpoint

--- training/src/anemoi/training/utils/checkpoint.py
+++ training/src/anemoi/training/utils/checkpoint.py
@@ -143,16 +143,19 @@ def freeze_submodule_by_name(module: nn.Module, target_name: str) -> None:
     module : torch.nn.Module
         Pytorch model
     target_name : str
-        The name of the submodule to freeze.
+        The name of the submodule to freeze. Examples: "encoder", "encoder.lam".
     """
     for name, child in module.named_children():
         # If this is the target submodule, freeze its parameters
         if name == target_name:
             for param in child.parameters():
+                LOGGER.info("Freezing parameter: %s", param.shape)
                 param.requires_grad = False
+        elif target_name.startswith(name + "."):
+            new_target = target_name.replace(name + ".", "", 1)
+            freeze_submodule_by_name(child, new_target)
         else:
-            # Recursively search within children
-            freeze_submodule_by_name(child, target_name)
+            LOGGER.info("Skipping submodule: %s", name)
 
 
 class LoggingUnpickler(pickle.Unpickler):
