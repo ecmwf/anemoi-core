23bdd1a5efd4db0dbd7a6f27cf467ce7750b662d
devillerb <benjamin.devillers@meteo.fr>
2026-08-24 17:16:05 +0200

    feat(training): add config migrations

---
+++ training/src/anemoi/training/migrations/config.py
@@ -0,0 +1,46 @@
+# (C) Copyright 2026 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+from collections.abc import Generator
+from os import PathLike
+from pathlib import Path
+from typing import Any
+
+from anemoi.training.migrations.document import Document
+from anemoi.training.migrations.document import Ops
+from anemoi.training.migrations.interpolations import InterpolationReferences
+
+
+class Config:
+    def __init__(self, path: PathLike | str) -> None:
+        self._path = Path(path)
+        self._interpolations = InterpolationReferences()
+        self._ops: list[tuple[Ops, tuple[Any, ...]]] = []
+
+    def documents(self) -> Generator[Document, None, None]:
+        for path in self._path.glob("**/*.yaml"):
+            yield Document(path, path.relative_to(self._path).parts[:-1])
+
+    def parse_interpolations(self) -> None:
+        for document in self.documents():
+            self._interpolations.parse_document(document)
+
+    def drop_key(self, keys: str) -> None:
+        self._ops.append((Ops.DROP, (keys,)))
+
+    def add_key(self, keys: str, value: Any) -> None:
+        self._ops.append((Ops.ADD, (keys, value)))
+
+    def rename_key(self, start: str, end: str) -> None:
+        self._ops.append((Ops.RENAME, (start, end)))
+
+    def exec_ops(self) -> None:
+        for document in self.documents():
+            document.exec_ops(self._ops)
+        self._ops = []
