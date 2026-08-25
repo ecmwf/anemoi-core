17d722fabd925277d352fbed61598c3319b66794
devillerb <benjamin.devillers@meteo.fr>
2026-08-25 17:02:50 +0200

    rename document to config

--- training/src/anemoi/training/migrations/testing.py
+++ training/src/anemoi/training/migrations/testing.py
@@ -7,30 +7,15 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
-from collections.abc import Mapping
-from collections.abc import Sequence
 from pathlib import Path
 
 from anemoi.training.migrations.config import Config
-from anemoi.training.migrations.document import Document
 
 
-class DocumentFromContent:
+class ConfigFromContent:
     def __init__(self, path: Path) -> None:
         self._path = path
 
-    def __call__(self, content: str, prefix: Sequence[str] = ()) -> Document:
+    def __call__(self, content: str) -> Config:
         self._path.write_text(content)
-        return Document(self._path, prefix)
-
-
-class ConfigFromContents:
-    def __init__(self, path: Path) -> None:
-        self._path = path
-
-    def __call__(self, configs: Mapping[str, str]) -> Config:
-        for filename, content in configs.items():
-            path = self._path / filename
-            path.parent.mkdir(parents=True, exist_ok=True)
-            path.write_text(content)
         return Config(self._path)
