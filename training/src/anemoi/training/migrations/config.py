f39c0becfcbc0f0038915a975ccb3ba2098ad1d6
devillerb <benjamin.devillers@meteo.fr>
2026-08-25 16:57:37 +0200

    add selected documents

--- training/src/anemoi/training/migrations/config.py
+++ training/src/anemoi/training/migrations/config.py
@@ -7,6 +7,7 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
+import logging
 from collections.abc import Generator
 from functools import cached_property
 from os import PathLike
@@ -14,15 +15,15 @@ from pathlib import Path
 from typing import Any
 
 from anemoi.training.migrations.document import Document
-from anemoi.training.migrations.document import Ops
 from anemoi.training.migrations.interpolations import InterpolationReferences
+
+LOGGER = logging.getLogger(__name__)
 
 
 class Config:
     def __init__(self, path: PathLike | str) -> None:
         self._path = Path(path)
         self._interpolations = InterpolationReferences()
-        self._ops: list[tuple[Ops, tuple[Any, ...]]] = []
 
     @cached_property
     def documents(self) -> dict[str, Document]:
@@ -41,15 +42,23 @@ class Config:
         yield from self.documents.values()
 
     def drop_key(self, keys: str) -> None:
-        self._ops.append((Ops.DROP, (keys,)))
+        n_executed_ops = 0
+        for document in self.selected_documents():
+            if not keys.startswith(document.prefix):
+                continue
+            document.drop_key(keys.removeprefix(document.prefix))
+            n_executed_ops += 1
+        if n_executed_ops == 0:
+            LOGGER.warning("drop_key method did not change any config file.")
 
     def add_key(self, keys: str, value: Any) -> None:
-        self._ops.append((Ops.ADD, (keys, value)))
+        for document in self.selected_documents():
+            if not keys.startswith(document.prefix):
+                continue
+            document.add_key(keys.removeprefix(document.prefix), value)
 
     def rename_key(self, start: str, end: str) -> None:
-        self._ops.append((Ops.RENAME, (start, end)))
-
-    def exec_ops(self) -> None:
-        for document in self.documents.values():
-            document.exec_ops(self._ops)
-        self._ops = []
+        for document in self.selected_documents():
+            if not start.startswith(document.prefix) or not end.startswith(document.prefix):
+                continue
+            document.rename_key(start.removeprefix(document.prefix), end.removeprefix(document.prefix))
