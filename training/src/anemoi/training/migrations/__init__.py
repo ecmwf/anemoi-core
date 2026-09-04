55b272d6b91bfe6eaa16a8a16de74f2abf51f157
Benjamin Devillers <benjamin.devillers@meteo.fr>
2026-09-04 12:41:43 +0000

    export migration modules in __init__

--- training/src/anemoi/training/migrations/__init__.py
+++ training/src/anemoi/training/migrations/__init__.py
@@ -6,3 +6,16 @@
 # In applying this licence, ECMWF does not waive the privileges and immunities
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
+from anemoi.training.migrations.config import Config
+from anemoi.training.migrations.nodes import Node
+from anemoi.training.migrations.nodes import NodeContainer
+from anemoi.training.migrations.nodes import NodeDict
+from anemoi.training.migrations.nodes import NodeList
+
+__all__ = [
+    "Config",
+    "Node",
+    "NodeContainer",
+    "NodeDict",
+    "NodeList",
+]
