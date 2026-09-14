ea9cc173afaf6831234331ae5f4d9bbfa13ea558
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2026-09-14 14:21:11 +0000

    fix: import

--- graphs/tests/generate/test_mask_builder.py
+++ graphs/tests/generate/test_mask_builder.py
@@ -14,7 +14,7 @@ from scipy.spatial import cKDTree
 from torch_geometric.data import HeteroData
 
 from anemoi.graphs import EARTH_RADIUS
-from anemoi.graphs.generate.masks import PYG_LIB_AVAILABLE
+from anemoi.graphs.utils import is_pyg_lib_available
 from anemoi.graphs.generate.masks import AreaMaskBuilder
 
 
@@ -62,7 +62,7 @@ def test_fit(graph_with_nodes: HeteroData, mask: str):
     mask_builder.fit(graph_with_nodes)
 
     assert getattr(mask_builder._backend, "_ref_vectors") is not None
-    if PYG_LIB_AVAILABLE:
+    if is_pyg_lib_available():
         assert mask_builder._backend._ref_vectors.shape[1] == 3
         assert isinstance(mask_builder._backend._ref_vectors, torch.Tensor)
     else:
