3a39952c53e55260bdc3ca326addd5515baa3acf
OpheliaMiralles <opheliamiralles@gmail.com>
2026-09-21 15:13:03 +0300

    refactor(training): rename strategies to iterations

--- training/tests/integration/test_multidomain_data.py
+++ training/tests/integration/test_multidomain_data.py
@@ -13,7 +13,7 @@ from omegaconf import DictConfig
 
 from anemoi.training.data.datamodule import AnemoiDatasetsDataModule
 from anemoi.training.data.multidataset import MultiDataset
-from anemoi.training.data.iteration_strategy import CrossDatasetIterationStrategy
+from anemoi.training.data.iteration import CrossDatasetIteration
 from anemoi.training.schemas.base_schema import BaseSchema
 from anemoi.training.schemas.base_schema import convert_to_omegaconf
 from anemoi.utils.testing import GetTestArchive
@@ -33,7 +33,7 @@ def test_multidomain_dataloader(
     cfg = convert_to_omegaconf(BaseSchema(**cfg))
     datamodule = AnemoiDatasetsDataModule(cfg, instantiate(cfg.task))
     assert isinstance(datamodule.ds_train, MultiDataset)
-    assert isinstance(datamodule.ds_train.strategy, CrossDatasetIterationStrategy)
+    assert isinstance(datamodule.ds_train.iteration, CrossDatasetIteration)
 
     sampled_domains = set()
     grid_sizes = {}
@@ -54,6 +54,6 @@ def test_config_validation_multidomain(multidomain_config: tuple[DictConfig, lis
     cfg, _ = multidomain_config
     cfg = convert_to_omegaconf(BaseSchema(**cfg))
     assert (
-        cfg.dataloader.strategy._target_
-        == "anemoi.training.data.iteration_strategy.CrossDatasetIterationStrategy"
+        cfg.dataloader.iteration._target_
+        == "anemoi.training.data.iteration.CrossDatasetIteration"
     )
